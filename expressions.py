9e14142f8f53b84e8e8473971a6663e65c9ce722
Ari Majumdar <akm303@gwmail.gwu.edu>
2026-02-11 18:42:28 -0800

    implementing boolean objects, variables and literals; wrote a test for creating variables

---
+++ expressions.py
@@ -0,0 +1,113 @@
+from itertools import count
+
+# -------------------------- #
+#          Objects           #
+# -------------------------- #
+
+
+class Variable:
+    id_counter = count()
+
+    def __init__(self, title: str, value: bool):
+        assert isinstance(title, str)
+        assert isinstance(value, bool)
+        self.id = next(Variable.id_counter)
+        self.title = title
+        self.val = value
+
+    def hasValue(self):
+        return self.val in [True, False]
+
+    def __str__(self):
+        return f"V_{self.id}::[{self.title}={int(self.val)}]"   # `V_0::[A=1]`
+        # return f"V_{self.id}::{self.title}={int(self.val)}"   # `V_0::A=1`
+        # return f"V_{self.id}::{self.title}[{int(self.val)}]"    # `V_0::A[1]`
+
+
+class Literal(Variable):
+    """
+    A literal is an occurrence of a variable in either natural or complementary form
+    eg. in `(x_1'+x_2)(x_1'+x_3)`
+        - first literal in this expression is x_1'
+        - second literal in this expression is x_2'
+    """
+
+    def __init__(self, title, value):
+        super().__init__(title, value)
+
+
+# class Expression:
+#     def __init__(self,form,expr_str):
+#         self.form = form
+#         self.expr_str = expr_str
+
+
+# -------------------------- #
+#           tests            #
+# -------------------------- #
+def test_variable():
+    VAR_DATA_KEY = "variable"
+    TEST_PASSES_KEY = "test_passes"
+
+    all_tests = [
+        {VAR_DATA_KEY: ("A", True), TEST_PASSES_KEY: True},
+        {VAR_DATA_KEY: ("B", False), TEST_PASSES_KEY: True},
+        {VAR_DATA_KEY: ("C", 1), TEST_PASSES_KEY: False},
+        {VAR_DATA_KEY: ("D", 0), TEST_PASSES_KEY: False},
+        {VAR_DATA_KEY: ("E", None), TEST_PASSES_KEY: False},
+        {VAR_DATA_KEY: (5, True), TEST_PASSES_KEY: False},
+        {VAR_DATA_KEY: (5, 1), TEST_PASSES_KEY: False},
+    ]
+
+    variables = []
+    for test_data in all_tests:
+        var_data = test_data[VAR_DATA_KEY]
+        should_pass = test_data[TEST_PASSES_KEY]
+        
+        test_name = var_data[0]
+        var = None
+        try:
+            var = Variable(*var_data)
+            print(f"{test_name} Passes: created {var}")
+            varHasValue = var.hasValue()
+            print(f"{test_name} Passes: {var} has value? {varHasValue}")
+
+            variables.append(var)
+
+        except Exception as e:
+            if not should_pass and isinstance(e, AssertionError):
+                print(f"{test_name} Passes: assertion caught on Variable{var_data}")
+            else:
+                print(f"{test_name} Fails: Variable{var_data} not created")
+    return variables
+
+    # v1 = Variable("A", True)
+    # v1HasValue = v1.hasValue()
+    # print(f"v1: {v1}")
+    # print(f"has value?: {v1HasValue}")
+
+    # v2 = Variable("B", False)
+    # v2HasValue = v2.hasValue()
+    # print(f"v2: {v2}")
+    # print(f"has value?: {v2HasValue}")
+
+    # v3 = Variable("C", 1)
+    # v3HasValue = v3.hasValue()
+    # print(f"v3: {v3}")
+    # print(f"has value?: {v3HasValue}")
+
+    # v4 = Variable("D", 0)
+    # v4HasValue = v4.hasValue()
+    # print(f"v4: {v4}")
+    # print(f"has value?: {v4HasValue}")
+
+
+# def test_literal():
+
+if __name__ == "__main__":
+    result = test_variable()
+    print(list(map(str,result)))
+    # test_literal()
+
+
+# class CNFExpression(Expression):
--- satsolver.py
+++ satsolver.py
@@ -1,10 +1,59 @@
-class SAT:
+# from truthtables import *
+
+# def propositional
+
+# def toCNF(expression):
+
+
+class Expression:
+    def __init__(self, cnf_expression:str):
+        self.expression = cnf_expression
+        self.literals = []
+        self.operations = []
+        self.parse()
+
+    def parse(self):
+        expression = self.expression
+
+
+class Problem:
+    """
+    Abstract Problem Class
+    """
+    def __init__(self):
+        pass
+
+class SAT(Problem):
     """
     SAT Problem
     """
     def __init__(self):
-        pass    
+        self.expression = None
+
 
 class Solver:
-    def __init__(self):
-        pass
+    def __init__(self,problem,algorithm,autorun = False):
+        self.problem = problem
+        self.algorithm = algorithm
+        if autorun:
+            self.solution = self.solve()
+    
+    def solve(self):
+        self.algorithm(self.problem)
+
+
+def expression_test():
+    cnf_e1 = ""
+    e1 = Expression()
+
+def sat_test():
+    pass
+
+def solver_test():
+    pass
+
+
+if __name__ == "__main__":
+    expression_test()
+    sat_test()
+    solver_test()
