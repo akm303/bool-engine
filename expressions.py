b0ed13cb76f589fb323f7bcde929400d89d83fad
Ari Majumdar <akm303@gwmail.gwu.edu>
2026-02-11 19:12:27 -0800

    added comments and some cleanup on test printouts and variable printing to stdout

--- expressions.py
+++ expressions.py
@@ -4,6 +4,7 @@ from itertools import count
 #          Objects           #
 # -------------------------- #
 
+INT_ID = True
 
 class Variable:
     id_counter = count()
@@ -11,17 +12,26 @@ class Variable:
     def __init__(self, title: str, value: bool):
         assert isinstance(title, str)
         assert isinstance(value, bool)
-        self.id = next(Variable.id_counter)
+        if INT_ID:
+            self.id = next(Variable.id_counter) #id is an int
+        else:
+            self.id = f"V_{next(Variable.id_counter)}" #id is a str
         self.title = title
-        self.val = value
+        self.value = value
 
     def hasValue(self):
-        return self.val in [True, False]
+        return self.value in [True, False]
+
+    def toDict(self):
+        return {self.id:{self.title:self.value}}
 
     def __str__(self):
-        return f"V_{self.id}::[{self.title}={int(self.val)}]"   # `V_0::[A=1]`
-        # return f"V_{self.id}::{self.title}={int(self.val)}"   # `V_0::A=1`
-        # return f"V_{self.id}::{self.title}[{int(self.val)}]"    # `V_0::A[1]`
+        var_id = f"V_{self.id}" if INT_ID else f"{self.id}"
+
+        return f"{var_id}::[{self.title}={int(self.value)}]"   # `V_0::[A=1]`
+        # return f"{var_id}::{self.title}={int(self.value)}"   # `V_0::A=1`
+        # return f"{var_id}::{self.title}[{int(self.value)}]"    # `V_0::A[1]`
+
 
 
 class Literal(Variable):
@@ -45,19 +55,24 @@ class Literal(Variable):
 # -------------------------- #
 #           tests            #
 # -------------------------- #
+def bar():
+    print('-'*40)
+
+
 def test_variable():
     VAR_DATA_KEY = "variable"
     TEST_PASSES_KEY = "test_passes"
 
+    # test order matters
     all_tests = [
-        {VAR_DATA_KEY: ("A", True), TEST_PASSES_KEY: True},
-        {VAR_DATA_KEY: ("B", False), TEST_PASSES_KEY: True},
-        {VAR_DATA_KEY: ("C", 1), TEST_PASSES_KEY: False},
-        {VAR_DATA_KEY: ("D", 0), TEST_PASSES_KEY: False},
-        {VAR_DATA_KEY: ("E", None), TEST_PASSES_KEY: False},
-        {VAR_DATA_KEY: (5, True), TEST_PASSES_KEY: False},
-        {VAR_DATA_KEY: (6, 1), TEST_PASSES_KEY: False},
-        {VAR_DATA_KEY: ("H", True), TEST_PASSES_KEY: True},
+        {VAR_DATA_KEY: ("A", True), TEST_PASSES_KEY: True}, # valid
+        {VAR_DATA_KEY: ("B", False), TEST_PASSES_KEY: True},# valid
+        {VAR_DATA_KEY: ("C", 1), TEST_PASSES_KEY: False},   # invalid because value must be a bool
+        {VAR_DATA_KEY: ("D", 0), TEST_PASSES_KEY: False},   # invalid because value must be a bool
+        {VAR_DATA_KEY: ("E", None), TEST_PASSES_KEY: False},# invalid because value must be a bool
+        {VAR_DATA_KEY: (5, True), TEST_PASSES_KEY: False},  # invalid because variable title must be string
+        {VAR_DATA_KEY: (6, 1), TEST_PASSES_KEY: False},     # invalid because variable title must be string
+        {VAR_DATA_KEY: ("H", True), TEST_PASSES_KEY: True}, # valid (check id counter)
     ]
 
     variables = []
@@ -67,11 +82,12 @@ def test_variable():
         
         test_name = var_data[0]
         var = None
+        bar()
         try:
             var = Variable(*var_data)
-            print(f"{test_name} Passes: created {var}")
-            varHasValue = var.hasValue()
-            print(f"{test_name} Passes: {var} has value? {varHasValue}")
+            print(f"{test_name} Passes:")
+            print(f"        : created {var}")
+            print(f"        : {var} has value? {var.hasValue()}")
 
             variables.append(var)
 
@@ -80,6 +96,8 @@ def test_variable():
                 print(f"{test_name} Passes: assertion caught on Variable{var_data}")
             else:
                 print(f"{test_name} Fails: Variable{var_data} not created")
+
+    bar()
     return variables
 
 
