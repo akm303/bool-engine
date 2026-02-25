e81a93975eecc76e471c57b6ceaab997b36c1850
Ari Majumdar <akm303@gwmail.gwu.edu>
2026-02-25 09:06:39 -0800

    first pass complete w backgracking, not correct; suspect is_consistent is wrong

--- src/csp.py
+++ src/csp.py
@@ -12,64 +12,81 @@ from pprint import pformat
 
 # CSP Problem definition
 class CSP:
-    def __init__(self, variables: list, domains: list, constraints: list):
+    def __init__(self, variables: list, domains: list, constraints: list, algorithm):
         self.variables: list = variables
         self.domains: list = domains
         self.constraints: list = constraints
-        # self.algorithm = algorithm
+        self.algorithm = algorithm
         self.solution = None
 
     def solve(self):
         assignment = {}
-        self.solution = self.backtrack(assignment)
+        print(f"self.variables: {self.variables}")
+        print(f"self.domains: {self.domains}")
+        print(f"assignment (pre): {assignment}")
+        self.solution = self.algorithm(
+            self.variables, self.domains, self.constraints, assignment
+        )
+        print(f"assignment (post): {assignment}")
         return self.solution
 
-    def backtrack(self, assignment):
-        if len(assignment) == len(self.variables):
-            return assignment
 
-        var = self.select_unassigned_variable(assignment)
-        for value in self.order_domain_values(var, assignment):
-            if self.is_consistent(var, value, assignment):
-                assignment[var] = value
-                result = self.backtrack(assignment)
-                if result is not None:
-                    return result
-                del assignment[var]
-        return None
-
-    def select_unassigned_variable(self, assignment):
-        unassigned_vars = [var for var in self.variables if var not in assignment]
-        return min(unassigned_vars, key=lambda var: len(self.domains[var]))
-
-    def order_domain_values(self, var, assignment):
-        return self.domains[var]
-
-    def is_consistent(self, var, value, assignment):
-        for constraint_var in self.constraints[var]:
-            if constraint_var in assignment and assignment[constraint_var] == value:
-                return False
-        return True
+# Backtrack & helper functions
+def is_complete(variables: list, assignment: dict):
+    print(f"is_complete: variables {variables}, assignment {assignment}")
+    return len(assignment) == len(variables)
 
 
-# Backtrack helper functions
-def select_unassigned_variable(self, assignment):
-    unassigned_vars = [var for var in self.variables if var not in assignment]
-    return min(unassigned_vars, key=lambda var: len(self.domains[var]))
+def select_unassigned_variable(variables: list, domains: dict, assignment: dict):
+    print()
+    unassigned_vars = [var for var in variables if var not in assignment]
+    print(
+        f"assignment: {assignment}, variables: {variables}, unassigned: {unassigned_vars}"
+    )
+    return min(unassigned_vars, key=lambda var: len(domains[var]))
+    # unassigned_vars = [var for var in variables if var not in assignment]
+    # return min(unassigned_vars, key=lambda var: len(domains[var]))
 
 
-def order_domain_values(self, var, assignment):
-    return self.domains[var]
+def order_domain_values(var: str, domains: dict, assignment: dict):
+    return domains[var]
 
 
-def is_consistent(self, var, value, assignment):
-    for constraint_var in self.constraints[var]:
-        if constraint_var in assignment and assignment[constraint_var] == value:
-            return False
-    return True
+def is_consistent(var: str, value: int, constraints: dict, assignment: dict) -> bool:
+    # @param {dict} constraints: {var:complement(var)}
+    #       contains var's complement. if var and its complement assigned the same value, false
+    # valid_complements: bool = (constraints[var] in assignment) and (
+    #     assignment[constraints[var]] == assignment[var]
+    # )
+    valid_complements: bool = (constraints[var] in assignment) and (
+        value == assignment[var]
+    )
+    print(f"valid complements? {valid_complements} (var={var},val={value},constraints={constraints},assignment={assignment})")
+    return valid_complements  # and evaluates_to_true
+
+
+def backtrack(variables: list, domains: dict, constraints: dict, assignment: dict):
+    if is_complete(variables, assignment):
+        return assignment
+
+    var = select_unassigned_variable(variables, domains, assignment)
+    for value in order_domain_values(var, domains, assignment):
+        if is_consistent(var, value, constraints, assignment):
+            assignment[var] = value
+            result = backtrack(variables, domains, assignment)
+            if result is not None:
+                return result
+            del assignment[var]
+    return None
 
 
 # --------------------------------------------------- #
+
+
+def output_result(result):
+    """output solution to result.json"""
+    with open("results.json", "w") as json_fp:
+        json.dump(result, json_fp, indent=4)
 
 
 cnf_test_expressions = [
@@ -79,44 +96,32 @@ cnf_test_expressions = [
 ]
 
 
-def output_result(result):
-    """output solution to result.json"""
-    with open("results.json", "w") as json_fp:
-        json.dump(result, json_fp, indent=4)
-
+def order(var_set: set):
+    return sorted(list(var_set))  # eg. (v1,v1',v2,v2',...)
 
 
 def parse_input(input_str: str):
     """parse input expression into variables"""
-    variable_pattern = r"(\w+'?)"  # r"(x_\d+'?)"
+    expression = input_str.replace(" ", "")
+
+    var_pattern = r"(\w+'?)"  # r"(x_\d+'?)"
     clause_pattern = r"\([^()]+\)"
 
-    expression = input_str.replace(" ", "")
-    variables = re.findall(variable_pattern, expression)
     clauses = re.findall(clause_pattern, expression)
-
-    out_str = (
-        f'\nexpression     ({type(expression)}) :  "{expression}"'
-        f"\nvariable_match ({type(variables)}): {variables}"
-        f"\nclause_match   ({type(clauses)}): {clauses}"
-    )
-    print(out_str)
-    return {
-        "expression": input_str,
-        "clauses": clauses,
-        "variables": variables,
-    }
+    clauses = {i: re.findall(var_pattern, clause) for i, clause in enumerate(clauses)}
+    return clauses
 
 
-def get_complement(var):
+def complement(var):
     var_is_complement = "'" in var
-    return var[:-1] if var_is_complement else var+"'"
+    return var[:-1] if var_is_complement else var + "'"
 
-def add_complements(variables:set, complements:dict):
+
+def add_complements(variables: set, complements: dict):
     for var1 in variables:
         # associate var with its complement
-        var2 = get_complement(var1)
-        print(f"{var1:4}s complement is {var2:4}",end=" ")
+        var2 = complement(var1)
+        print(f"{var1:4}s complement is {var2:4}", end=" ")
         if var2 in variables:
             print("(in variables)")
             complements[var1] = var2
@@ -124,32 +129,48 @@ def add_complements(variables:set, complements:dict):
             print("(not in variables)")
 
 
-# def add_constraints(variables, constraints: dict):
-#     for var1 in variables:
-#         # associate var with its complement
-#         var2 = get_complement(var1)
-#         if var2 in variables:
-#             constraints[var1].append(var2)
-#             print(f"added {var2} to {var1} constraints")
-
-    # constraints[var] = []
-    # for i in range(9):
-    #     if i != var[0]:
-    #         constraints[var].append((i, var[1]))
-    #     if i != var[1]:
-    #         constraints[var].append((var[0], i))
-    # sub_i, sub_j = var[0] // 3, var[1] // 3
-    # for i in range(sub_i * 3, (sub_i + 1) * 3):
-    #     for j in range(sub_j * 3, (sub_j + 1) * 3):
-    #         if (i, j) != var:
-    #             constraints[var].append((i, j))
-
-
-# for expr in cnf_test_expressions:
-#     parse_input(expr)
-
-
 def main():
+    # input and parse expression data
+    input_str = cnf_test_expressions[2]
+    clause_dict = parse_input(input_str)
+
+    var_set = set()
+    for clause_id, variables in clause_dict.items():
+        print(f"clause {clause_id}: variables [{', '.join(variables)}]")
+        for var in variables:
+            var_set.add(var)
+
+    ordered_vars = order(var_set)
+    print(f"var_set: {var_set}")
+    print(f"order(var_set): {ordered_vars}")
+
+    complements = {}
+    for var in var_set:
+        var_complement = complement(var)
+        complements[var] = var_complement if var_complement in var_set else None
+    print(f"complements: {complements}")
+
+    # constraints = {}
+    # for v,vc in complements.items():
+    #     constraints[v] = []
+    #     if vc:
+    #         constraints[v].append((vc,0))
+    #         constraints[v].append((vc,1))
+
+    # print(f"constraints: {constraints}")
+
+    base_domain = (0, 1)  # (True,False)
+
+    # init results
+    domains = {var: base_domain for var in var_set}
+
+    csp = CSP(ordered_vars, domains, complements, backtrack)
+    # csp = CSP(ordered_vars,domains,constraints)
+    solution = csp.solve()
+    print(f"\nsolution: {solution}")
+
+
+def main_1():
     # input and parse expression data
     input_str = cnf_test_expressions[2]
     input_data = parse_input(input_str)
@@ -182,8 +203,8 @@ def main():
     print(f"var set  : {var_set}")
     print(f"domains:\n{pformat(domain_dict,indent=2)}")
 
-    active_complements = {var:None for var in var_set}
-    add_complements(var_set,active_complements)
+    active_complements = {var: None for var in var_set}
+    add_complements(var_set, active_complements)
     print(f"complement_dict: {active_complements}")
 
 
