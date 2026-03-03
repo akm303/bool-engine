c9db0dfe55a3a80100eb918be4120400699101af
Ari Majumdar <akm303@gwmail.gwu.edu>
2026-03-03 07:18:23 -0800

    works; fixed is_consistent and value retrieval for literal based on assignment and current var/val to-be-assigned

--- src/sat.py
+++ src/sat.py
@@ -17,10 +17,10 @@ class CSP:
 
     def solve(self, algorithm):
         assignment = {}
-        # print(f"variables: {self.variables}")
-        # print(f"domains: {self.domains}")
-        # print(f"constraints:\n{pformat(self.constraints)}")
-        # print()
+        print(f"variables: {self.variables}")
+        print(f"domains: {self.domains}")
+        print(f"constraints:\n{pformat(self.constraints)}")
+        print()
         self.solution = algorithm(
             self.variables, self.domains, self.constraints, assignment
         )
@@ -46,37 +46,39 @@ def select_unassigned_variable(variables: list, domains: dict, assignment: dict)
 def order_domain_values(var: str, domains: dict, assignment: dict):
     return domains[var]
 
-def contains_one(clause:dict):
-    return any(clause.values())
+
+def contains_one_or_none(clause: list, var, val, assignment: dict):
+    # cvalues = clause.values()
+    # return all(v in [True, None] for v in cvalues)
+    return all(value_of(lit,var,val,assignment) in [True, None] for lit in clause)
+
+
+def value_of(lit: str, var: str, val: str, assignment: dict):
+    bvar = base_var(lit)
+    bval = val if bvar == var else assignment.get(bvar, None)
+    rval = bval if not is_complement(lit) else not bval
+    return rval
+
 
 def is_consistent(var: str, val: int, constraints: dict, assignment: dict) -> bool:
-    print(f"\nis_consistent({assignment}) with {{{var}:{val}}}")
+    """check if var assignment to val is consistent with rest of assignment and constraints"""
     clauses = constraints[var]
-    for clause in clauses:
-        print(f"gathering assignment for {clause}...")
-        rclause = package_clause(var,val,assignment,clause)
-        print(f"  result Clause: {rclause}")
-        clause_assignment = {literal: get_value(literal, var, val, assignment) for literal in clause}
-        # clause_assignment = [value(var, val, cvar, assignment) for cvar in clause]
-        print(f"  eval(clause={clause}, A={assignment}+{{{var}:{val}}}) => Clause_Assignment={clause_assignment}")
-        # clause_result = evaluate_clause(clause_assignment)
-
-        clause_result = eval_clause(clause_assignment)
-        print(f"  clause result: {clause_result}")
-
-    return True
+    results = [contains_one_or_none(clause, var, val, assignment) for clause in clauses]
+    print(f"any(results): {any(results)} <- {results}")
+    return all(results)
 
 
 def backtrack(variables: list, domains: dict, constraints: dict, assignment: dict):
     print("-" * 40)
-    print(f"backtrack(assignment) -> {assignment}")
+    print(f"backtrack(A={assignment})")
+
     if is_complete(variables, assignment):
         return assignment
 
     var = select_unassigned_variable(variables, domains, assignment)
     for value in order_domain_values(var, domains, assignment):
         if is_consistent(var, value, constraints, assignment):
-            print(f"assigning {var}:{value} in {assignment}", end=" => ")
+            print(f"assigning {{{var}:{value}}} -> A={assignment}", end=" => ")
             assignment[var] = value
             print(f"{assignment}")
 
@@ -91,110 +93,53 @@ def backtrack(variables: list, domains: dict, constraints: dict, assignment: dic
 # --------------------------------------------------- #
 
 
+def is_complement(lit: str) -> bool:
+    return "'" in lit
+
+
+def base_var(lit: str) -> str:
+    return lit.replace("'", "")
+
+
+def eval_clause(clause_assignment: dict) -> bool:
+    print(f"eval {clause_assignment}")
+    if any(val in [1, None] for val in clause_assignment):
+        return True
+    return False
+
+
+# ------------------------------------------ #
+
+
 def output_result(result):
     """output solution to result.json"""
     with open("results.json", "w") as json_fp:
         json.dump(result, json_fp, indent=4)
 
 
-def order(var_set: set[str]) -> list[str]:
+def var_order(var_set: set[str]) -> list[str]:
     return sorted(list(var_set))  # eg. (v1,v2,...)
-
-
-def is_complement(var: str) -> bool:
-    return "'" in var
-
-
-def complement(lit: str) -> str:
-    return lit[:-1] if is_complement(lit) else lit + "'"
-
-
-def base_var(lit: str) -> str:
-    return lit if not is_complement(lit) else lit[:-1]
-
-
-def package_clause(var: str, val: bool, assignment: dict, clause: list):
-    clause_var = {
-        literal: value if literal == base_var(literal) else not value
-        for literal, value in assignment.items()
-        if base_var(literal) in clause
-    }
-    print(f"clause variables: {clause_var}")
-    return clause_var
-    # clause_var[var]
-
-
-def get_value(literal: str, var: str, val: bool, assignment: dict):
-    """get value of the literal based on either the assignment or trial variable"""
-
-    print(
-        f"  get value(lit={literal:2}, (var={var}, val={val}, A={assignment})) from",
-        end=" ",
-    )
-    bvar = base_var(literal)
-    if bvar == var:
-        print(f"trial (lit={literal} -> {var}) with {{{var}:{val}}}")
-        rval = not val if is_complement(literal) else val
-        return rval
-
-    print(f"asgmt ({bvar} in {assignment}) => {{{bvar}:{assignment.get(bvar)}}}")
-    base_val = assignment.get(bvar)
-    if base_val != None:
-        rval = not val if is_complement(literal) else val
-        # return base_val if bvar == literal else not base_val
-    return
-
-
-# def literal_value(lit: str, assignment: dict) -> bool:
-#     var = base_var(lit)
-#     val = assignment.get(var)
-#     print(f"result: base({lit})={var} ({var}={val})")
-
-#     val = not assignment.get(var) if is_complement(lit) else assignment.get(var)
-#     print(f"result: {lit} value: {val} ({var}={assignment.get(var)})")
-#     return val
-
-
-def eval_clause(clause_assignment: dict) -> bool:
-    print(f"eval {clause_assignment}")
-    if any(clause_assignment) or (None in clause_assignment.values()):
-        return True
-    return False
-
-
-# def evaluate_clause(var:str, assignment:dict, clauses:dict[int,list[str]]):
-#     print(f"variable: {var}")
-#     print(f"assignment: {assignment}")
-#     print(f"clauses: {clauses}") # clauses with var literals
-#     for clause_id,clause_literals in clauses.items():
-#         literal_values = [assignment[literal] for literal in clause_literals]
-#         print(f"{clause_id} literal_values: {literal_values}")
-
-
-def evaluate_clause(clause_assignment: list) -> bool:
-    """evaluate clause in cnf form (ie. sum assigned clause variables)"""
-    value_sum = sum(clause_assignment)
-    print(f"  clause evaluation(): value_sum: {value_sum}")
-    return value_sum > 0
 
 
 def parse_input(input_str: str) -> dict[int, list[str]]:
     """parse input expression into variables"""
     expression = input_str.replace(" ", "")
 
-    literal_pattern = r"(\w+'?)"  # r"(x_\d+'?)"
+    lit_pattern = r"(\w+'?)"  # r"(x_\d+'?)"
     clause_pattern = r"\([^()]+\)"
 
     clauses = re.findall(clause_pattern, expression)
-    clauses = {
-        i: re.findall(literal_pattern, clause) for i, clause in enumerate(clauses)
-    }
+    clauses = {i: re.findall(lit_pattern, clause) for i, clause in enumerate(clauses)}
     return clauses
 
 
 cnf_test_expressions = [
+    "(A)",
+    "(A')",
+    "(A)(A')",
     "(X+Y)",
     "(X+Y')",
+    "(X'+Y)",
     "(X+Y)(X'+Y)",
     "(X+Y)(X'+Y')",
     "(x_1' + x_2)(x_1' + x_3)",
@@ -205,34 +150,39 @@ cnf_test_expressions = [
 
 def main():
     # input and parse expression data
-    input_str = cnf_test_expressions[1]
-    print(f"input: {input_str}")
-    clause_dict = parse_input(input_str)
+    for cnf_expr in cnf_test_expressions[:3]:
+        input_str = cnf_expr
+        print(f"input: {input_str}")
+        clause_dict = parse_input(input_str)
 
-    var_set: set[str] = set()
-    constraints = {}
-    for clause_id, literals in clause_dict.items():
-        print(f"clause {clause_id} variables: [{', '.join(literals)}]")
-        for var in literals:
-            base_var = var.replace("'", "")
-            var_set.add(base_var)
-            constraints[base_var] = [literals]
-    variables = order(var_set)
+        var_set: set[str] = set()
+        constraints = {}
+        for clause_id, literals in clause_dict.items():
+            print(f"  clause {clause_id} literals: [{', '.join(literals)}]")
+            for lit in literals:
+                bvar = base_var(lit)
+                var_set.add(bvar)
+                constraints[bvar] = constraints.get(bvar, []) + [literals]
+                print(f"constraint constr: {constraints}")
+        variables = var_order(var_set)
 
-    # set variable domains
-    # base_domain = (0, 1)  # (True,False)
-    base_domain = (True, False)  # (True,False)
-    domains = {var: base_domain for var in var_set}
+        # set variable domains
+        # base_domain = (0, 1)  # (True,False)
+        base_domain = (True, False)  # (True,False)
+        domains = {var: base_domain for var in var_set}
 
-    # constraints based on clauses
-    csp = CSP(variables, domains, constraints)
-    solution = csp.solve(backtrack)
-    print(f"\nsolution: {solution}")
+        # constraints based on clauses
+        csp = CSP(variables, domains, constraints)
+        solution = csp.solve(backtrack)
+        print(f"\nsolution: {solution}")
+        print()
+        print("-" * 40)
+        print("-" * 40)
 
 
 def main_1():
     # input and parse expression data
-    input_str = cnf_test_expressions[2]
+    input_str = cnf_test_expressions[1]
     input_data = parse_input(input_str)
 
     # load data into local objects
