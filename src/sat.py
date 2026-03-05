082cfda2cac6b0a0924cf638e8c76ebbb65f61cf
Ari Majumdar <akm303@gwmail.gwu.edu>
2026-03-04 22:02:21 -0800

    adjusted print statements and stringify of different components

--- src/sat.py
+++ src/sat.py
@@ -2,7 +2,7 @@
 Docstring for src.sat
 
 Custom designed and implemented algorithm to solve SAT for expressions in CNF form
-input: string expression in CNF 
+input: string expression in CNF
 - expression is a disjunction of clauses
 - each clause is a conjunction of literals
 - each literal is a variable or its complement
@@ -15,28 +15,28 @@ from pprint import pformat
 from typing import Tuple
 
 # type aliases
-expr_type = str
-clause_type = list[int]
-var_type = str
-lit_type = var_type
-assignment_type = dict
+e_type = str
+c_type = list[int]
+v_type = str
+l_type = v_type
+a_type = dict
 
 # --------------------------------------------------- #
 # fill remaining assignment variables with wildcard symbol '*'
 WITH_FILL = False
 
 
-def is_complement(literal: lit_type):
+def is_complement(literal: l_type) -> bool:
     return "'" in literal
 
 
-def base_variable(literal: lit_type):
+def base_variable(literal: l_type) -> v_type:
     return literal.replace("'", "")
 
 
 def parse_expression(
     expr_string: str,
-) -> Tuple[expr_type, list[var_type], list[lit_type], list[clause_type]]:
+) -> Tuple[e_type, list[v_type], list[l_type], list[c_type]]:
     expression = expr_string.replace(" ", "")
     # expression,target = expression.split("=")
     # print(f"expression={expression}")
@@ -48,8 +48,8 @@ def parse_expression(
     clauses = re.findall(clause_pattern, expression)
     clauses = [re.findall(lit_pattern, clause) for clause in clauses]
 
-    literals = set()
-    variables = set()
+    literals: set[l_type] = set()
+    variables: set[v_type] = set()
     for clause in clauses:
         for literal in clause:
             literals.add(literal)
@@ -69,57 +69,83 @@ def inverse(val: int) -> int:
         return None
     return 1 if val == 0 else 0
 
+# def c_str(clause,mode="CNF"):
+def c_str(clause):
+    return f"({f' + '.join(clause)})"
+
+def clist_str(clauses):
+    rstr = [c_str(clause) for clause in clauses]
+    return f"[ {', '.join(rstr)} ]"
+
+def v_str(variable_list):
+    return f"[ {f', '.join(variable_list)} ]"
+
+def a_str(assignment:dict):
+    if not assignment:
+        return None
+    rstr = [f"{k}:{v}" for k,v in assignment.items()]
+    rstr = f"{{ {', '.join(rstr)} }}"
+    return rstr
 
 def eval_clause(
-    clause: list[lit_type], assignment: assignment_type, indent_space
+    clause: list[l_type], assignment: a_type, indent: str
 ) -> list[int | None]:
     """returns tuple containing value of clause literals"""
     clause_values = []
+    # print(f"{indent}from assignment={assignment}")
     for literal in clause:
         bvar = base_variable(literal)
         bval = assignment.get(bvar, None)
         clause_values.append(bval if bvar == literal else inverse(bval))
-    print(
-        f"{indent_space}? eval clause: {clause} from assignment={assignment} => {clause}={clause_values}"
-    )
+    print(f"{indent}? eval clause {c_str(clause)} => {clause_values}")
     return clause_values
 
 
 def backtrack(
-    assignment: assignment_type,
-    variables: list[var_type],
-    clauses: list[clause_type],
-    indent=2,
+    assignment: a_type,
+    variables: list[v_type],
+    clauses: list[c_type],
+    i_space: int = 0,
 ):
-    print(f"{' '*(indent-2)}>> backtrack(A={assignment},V={variables},C={clauses})")
-    pindent = " " * indent
+    indent = " " * i_space
+    # print(f"{indent}>> backtrack (A={assignment}, V={variables}, C={clauses})")
+    print(f"{indent}>> backtrack(")
+    print(f"{indent} |   A = {a_str(assignment)},")
+    print(f"{indent} |   V = {v_str(variables)},")
+    print(f"{indent} |   C = {clist_str(clauses)},")
+    print(f"{indent} |___)")
 
-    unassigned_vars = sorted(list(set(variables) - set(assignment.keys())))
+    indent: str = " " * (i_space+2)
+    unassigned_vars: list[v_type] = sorted(
+        list(set(variables) - set(assignment.keys()))
+    )
 
-    if all(1 in eval_clause(c, assignment, pindent) for c in clauses):
+    if all(1 in eval_clause(c, assignment, indent) for c in clauses):
         # complete assignment, fill remaining var with wildcard and return result
         if WITH_FILL and unassigned_vars:
             for var in unassigned_vars:
                 assignment[var] = "*"
-        print(f"{pindent}solution found! -> A={assignment}")
+        print(f"{indent}solution found! -> A={a_str(assignment)}")
         return assignment
 
-    print(f"{pindent}unassigned: U={unassigned_vars}")
+    print(f"{indent}unassigned: U={unassigned_vars}", end=" :: ")
     if unassigned_vars:
         current_var = unassigned_vars.pop(0)
-        print(f"{pindent}current var: {current_var}")
+        print(f'-> var="{current_var}"')
+        # print(f"{indent}current var: {current_var}")
 
         assignment[current_var] = 1  # assign(var,1)
         while assignment[current_var] >= 0:
-            result = backtrack(assignment, variables, clauses, indent + 2)
+            result = backtrack(assignment, variables, clauses, i_space + 2)
             if result is not None:
+                print(f"{indent}<<")
                 return result
             assignment[current_var] -= 1
 
-        print(f"{pindent}no unattempted values; backtracking")
+        print(f"{indent}<< no unattempted values; removing {current_var} from assignment",end='')
         del assignment[current_var]
-    
-    print(f"{pindent}no unassigned variables; backtracking")
+
+    print(f"\n{indent[:-2]}<< no unassigned variables; backtracking")
     return None
 
 
@@ -146,16 +172,18 @@ def main():
         print("-" * 40)
         expression, variables, literals, clauses = parse_expression(cnf_expr)
         print(f"expression {i+1}: {expression}=1")
-        print(f"clauses:\n{pformat(clauses)}")
-        print(f" literals: {literals}")
-        print(f"variables: {variables}")
-        assignment = assignment_type()
+        print(f"  clauses: {clist_str(clauses)}")
+        # print(f"clauses:\n{pformat(clauses)}")
+        print(f" literals: {v_str(literals)}")
+        print(f"variables: {v_str(variables)}")
+        assignment = a_type()
 
         print()
         print(f"init assignment: {assignment}")
         result = backtrack(assignment, variables, clauses)
         print()
         print(f"solution: {result}")
+        # print(f"solution: {a_str(result)}")
 
         print("-" * 40)
         print()
